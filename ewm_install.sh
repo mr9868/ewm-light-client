0240eb9eabdc551fce96dc3ec687d275418f4972
Muhammad Ruly <116989757+mr9868@users.noreply.github.com>
2024-10-22 12:03:48 +0700

    Update ewm_install.sh

--- ewm_install.sh
+++ ewm_install.sh
@@ -14,20 +14,15 @@ if [[ "$pkey" = "" ]]; then
     elif [[ "$ipfsv" = "" ]]; then
     echo "Please put your ipfs version !"
 else
-sudo apt update -y && sudo apt upgrade -y && sudo apt install screen -y
-git clone https://github.com/covalenthq/ewm-das
+sudo apt update -y && sudo apt upgrade -y && sudo apt install screen -y && git clone https://github.com/covalenthq/ewm-das
 cd ewm-das
-wget -O go-latest.tar.gz https://go.dev/dl/go1.23.2.linux-amd64.tar.gz && sudo tar -C /usr/local -xzf go-latest.tar.gz 
-echo "" >> ~/.bashrc
+wget -O go-latest.tar.gz https://go.dev/dl/go1.23.2.linux-amd64.tar.gz && sudo tar -C /usr/local -xzf go-latest.tar.gz && echo "" >> ~/.bashrc
 echo 'export GOPATH=$HOME/go' >> ~/.bashrc
 echo 'export GOROOT=/usr/local/go' >> ~/.bashrc
 echo 'export GOBIN=$GOPATH/bin' >> ~/.bashrc
 echo 'export PATH=$PATH:/usr/local/go/bin:$GOBIN' >> ~/.bashrc
 source ~/.bashrc
-go install honnef.co/go/tools/cmd/staticcheck@latest && make deps && make  && sudo bash install-trusted-setup.sh
-bash -c "wget -O ipfs-latest.tar.gz https://dist.ipfs.tech/kubo/v0."$ipfsv".0/kubo_v0."$ipfsv".0_linux-amd64.tar.gz && tar -xvzf ipfs-latest.tar.gz && sudo bash kubo/install.sh"
-screen -S ipfs -dm bash -c "ipfs daemon --init"   
-screen -S covalent -dm bash -c "./bin/light-client --rpc-url wss://coordinator.das.test.covalentnetwork.org/v1/rpc --collect-url https://us-central1-covalent-network-team-sandbox.cloudfunctions.net/ewm-das-collector --private-key "$pkey""
+go install honnef.co/go/tools/cmd/staticcheck@latest && make deps && make  && sudo bash install-trusted-setup.sh && bash -c "wget -O ipfs-latest.tar.gz https://dist.ipfs.tech/kubo/v0."$ipfsv".0/kubo_v0."$ipfsv".0_linux-amd64.tar.gz && tar -xvzf ipfs-latest.tar.gz && sudo bash kubo/install.sh" && screen -S ipfs -dm bash -c "ipfs daemon --init" && screen -S covalent -dm bash -c "./bin/light-client --rpc-url wss://coordinator.das.test.covalentnetwork.org/v1/rpc --collect-url https://us-central1-covalent-network-team-sandbox.cloudfunctions.net/ewm-das-collector --private-key $pkey"
 rm -rf go-latest.tar.gz
 rm -rf ipfs-latest.tar.gz
 clear;
