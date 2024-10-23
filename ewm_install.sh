6e0cfa0870fce0818045720a8a08b4cf5b5ca2ac
Muhammad Ruly <116989757+mr9868@users.noreply.github.com>
2024-10-23 11:25:27 +0700

    Update ewm_install.sh

--- ewm_install.sh
+++ ewm_install.sh
@@ -1,6 +1,7 @@
 clear;
 rm -rf ewm-das;
 pkill -f "ipfs";
+pkill -f "covalent";
 sudo rm -rf /usr/local/bin/ipfs;
 function myHeader(){
 echo -e "========================================"
@@ -17,7 +18,7 @@ if [[ "$pkey" = "" ]]; then
     elif [[ "$ipfsv" = "" ]]; then
     echo "Please put your ipfs version !"
 else
-sudo apt update -y && sudo apt upgrade -y && sudo apt install screen -y && git clone https://github.com/covalenthq/ewm-das && cd ewm-das && pwd && sudo cp -r bin/light-client /usr/local/bin/light-client && sudo bash install-trusted-setup.sh;
+sudo apt update -y && sudo apt upgrade -y && sudo apt install screen -y && git clone https://github.com/covalenthq/ewm-das && cd ewm-das && pwd && sudo cp -r ewm-das/bin/light-client /usr/local/bin/light-client && sudo bash install-trusted-setup.sh;
 function installGo(){
 wget -O go-latest.tar.gz https://go.dev/dl/go1.23.2.linux-amd64.tar.gz && sudo tar -C /usr/local -xzf go-latest.tar.gz && echo "" >> ~/.bashrc
 echo 'export GOPATH=$HOME/go' >> ~/.bashrc
