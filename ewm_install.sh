316f63ac3f07ba7960997b59838535d6b99dd4da
Muhammad Ruly <116989757+mr9868@users.noreply.github.com>
2024-10-23 12:10:11 +0700

    Update ewm_install.sh

--- ewm_install.sh
+++ ewm_install.sh
@@ -1,8 +1,12 @@
+# WELCOME, don't forget to leave a star to my project :) mr9868
+# Make sure there is nothing complicated
 clear;
 rm -rf ewm-das;
 pkill -f "ipfs";
 pkill -f "covalent";
 sudo rm -rf /usr/local/bin/ipfs;
+
+# My Header dunction
 function myHeader(){
 echo -e "========================================"
 echo -e  "=    EWM light-client auto installer   ="
@@ -18,17 +22,31 @@ if [[ "$pkey" = "" ]]; then
     elif [[ "$ipfsv" = "" ]]; then
     echo "Please put your ipfs version !"
 else
-sudo apt update -y && sudo apt upgrade -y && sudo apt install screen -y && git clone https://github.com/covalenthq/ewm-das && cd ewm-das && sudo bash install-trusted-setup.sh;
+
+# Installing required package
+sudo apt update -y && 
+sudo apt upgrade -y && 
+sudo apt install screen -y && 
+git clone https://github.com/covalenthq/ewm-das && 
+cd ewm-das && 
+sudo bash install-trusted-setup.sh;
+
+# Go install function
 function installGo(){
-wget -O go-latest.tar.gz https://go.dev/dl/go1.23.2.linux-amd64.tar.gz && sudo tar -C /usr/local -xzf go-latest.tar.gz && echo "" >> ~/.bashrc
+wget -O go-latest.tar.gz https://go.dev/dl/go1.23.2.linux-amd64.tar.gz && 
+sudo tar -C /usr/local -xzf go-latest.tar.gz && 
+echo "" >> ~/.bashrc
 echo 'export GOPATH=$HOME/go' >> ~/.bashrc
 echo 'export GOROOT=/usr/local/go' >> ~/.bashrc
 echo 'export GOBIN=$GOPATH/bin' >> ~/.bashrc
 echo 'export PATH=$PATH:/usr/local/go/bin:$GOBIN' >> ~/.bashrc
 source ~/.bashrc
 }
+
+# Check if go is installed on machine or not
 command -v go >/dev/null 2>&1 || { echo >&2 "Go is not found on this machine, Installing go ... "; sleep 5;installGo;}
 
+# Check if installed go is not outdated
 v=`go version | { read _ _ v _; echo ${v#go}; }`
 IFS="." tokens=( ${v} );
 version=${tokens[1]};
@@ -37,8 +55,26 @@ else echo "Your go version '"$version"' is updated, Next step ...";sleep 5;
 fi
 unset IFS;
 
-go install honnef.co/go/tools/cmd/staticcheck@latest && make deps && make  && sudo bash install-trusted-setup.sh && bash -c "wget -O ipfs-latest.tar.gz https://dist.ipfs.tech/kubo/v0."$ipfsv".0/kubo_v0."$ipfsv".0_linux-amd64.tar.gz && tar -xvzf ipfs-latest.tar.gz && sudo bash kubo/install.sh" && source ~/.bashrc && screen -dmS ipfs bash -c "ipfs daemon --init;exec bash;" && sudo cp -r bin/light-client /usr/local/bin/light-client && screen -dmS covalent bash -c "sudo light-client --rpc-url wss://coordinator.das.test.covalentnetwork.org/v1/rpc --collect-url https://us-central1-covalent-network-team-sandbox.cloudfunctions.net/ewm-das-collector --private-key $pkey;exec bash"
-rm -rf go-latest.tar.gz && rm -rf ipfs-latest.tar.gz
+# Installing required Go packages
+go install honnef.co/go/tools/cmd/staticcheck@latest && 
+make deps &&
+make  && 
+sudo bash install-trusted-setup.sh &&
+
+# Installing ipfs kubo
+bash -c "wget -O ipfs-latest.tar.gz https://dist.ipfs.tech/kubo/v0."$ipfsv".0/kubo_v0."$ipfsv".0_linux-amd64.tar.gz" &&
+tar -xvzf ipfs-latest.tar.gz &&
+sudo bash kubo/install.sh && 
+source ~/.bashrc && 
+screen -dmS ipfs bash -c "ipfs daemon --init;exec bash;" && 
+
+# Installing covalent light-client node
+sudo cp -r bin/light-client /usr/local/bin/light-client && 
+screen -dmS covalent bash -c "sudo light-client --rpc-url wss://coordinator.das.test.covalentnetwork.org/v1/rpc --collect-url https://us-central1-covalent-network-team-sandbox.cloudfunctions.net/ewm-das-collector --private-key $pkey;exec bash"
+rm -rf go-latest.tar.gz &&
+rm -rf ipfs-latest.tar.gz
+
+# Welldone ! 
 clear;
 myHeader;
 echo "SETUP INSTALLED SUCCESSFULLY !"
