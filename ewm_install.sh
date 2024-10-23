5345564c76600cff53d2988c8f187d8391d62a02
Muhammad Ruly <116989757+mr9868@users.noreply.github.com>
2024-10-24 01:09:47 +0700

    Update ewm_install.sh

--- ewm_install.sh
+++ ewm_install.sh
@@ -62,6 +62,7 @@ read -p "Input your hexadecimal Private Keys ( without 0x ) : " pkey
 entryPointPK;
 read -p "Choose ipfs version (29/30/31) : " ipfsv
 entryPointIPFS;
+myHeader;
 echo
 echo "==================== INSTALLATION START ===================="
 echo
