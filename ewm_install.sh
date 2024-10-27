9c04683d20c304f3e56cfea5553852204c93cfb9
Muhammad Ruly <116989757+mr9868@users.noreply.github.com>
2024-10-27 16:05:44 +0700

    Update ewm_install.sh

--- ewm_install.sh
+++ ewm_install.sh
@@ -134,8 +134,8 @@ while sleep 10;
 do
 for i in \$(seq 1 $loop);
 do       
-logMsg=\"cat covalent\"\$i\".log | grep 'verified=true'
-ipfsMsg=\$(eval 'cat ipfs.log' | grep 'ERROR');   
+logMsg=\"cat covalent\"\$i\".log | grep 'verified=true'\"
+ipfsMsg=\$(eval \"cat ipfs.log | grep 'ERROR'\");   
 curl -s -X POST https://api.telegram.org/bot\$API_TOKEN/sendMessage -d chat_id=\$CHAT_ID -d text=\"\$ipfsMsg\"
 
 # Set the message text                     
