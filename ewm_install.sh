7586337c4dbadce7646325500309fa82161f3712
Muhammad Ruly <116989757+mr9868@users.noreply.github.com>
2024-10-27 16:45:01 +0700

    Update ewm_install.sh

--- ewm_install.sh
+++ ewm_install.sh
@@ -129,12 +129,11 @@ API_TOKEN=$tgApiQn
 CHAT_ID=$tgIdQn
 MESSAGE=\$(eval \"cat ipfs.log\");   
 curl -s -X POST https://api.telegram.org/bot\$API_TOKEN/sendMessage -d chat_id=\$CHAT_ID -d text=\"\$MESSAGE\"
-
 while sleep 10;
 do
 for i in \$(seq 1 $loop);
 do       
-logMsg=\$(eval \"cat covalent\"\$i\".log | grep 'verified=true'\"
+logMsg=\$(eval \"cat covalent\"\$i\".log | grep 'verified=true'\")
 ipfsMsg=\$(eval \"cat ipfs.log | grep 'ERROR'\");   
 curl -s -X POST https://api.telegram.org/bot\$API_TOKEN/sendMessage -d chat_id=\$CHAT_ID -d text=\"\$ipfsMsg\"
 
