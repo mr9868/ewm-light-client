c0dd325f47f5751098ca33419b13684e20deb99f
Muhammad Ruly <116989757+mr9868@users.noreply.github.com>
2024-10-28 00:26:52 +0700

    Update ewm_install.sh

--- ewm_install.sh
+++ ewm_install.sh
@@ -129,14 +129,13 @@ function tgMsg(){
 API_TOKEN=$tgApiQn   
 CHAT_ID=$tgIdQn
 MESSAGE=\$(eval \" cat ipfs.log\"); 
-MESSAGE+=\"\nPlease wait for 1-2 hours\";
 curl -s -X POST https://api.telegram.org/bot\$API_TOKEN/sendMessage -d chat_id=\$CHAT_ID -d text=\"\$MESSAGE\"
 sleep 30;
 for akun in \$(seq 1 $loop);
 do  
-msgStart=\$(eval \" cat covalent\"\$akun\".log | awk '{print tolower(\$0)}' | grep -ow '\w*0x\w*'\")
-accStart=\$(eval \" echo 'Address \$akun : \`\$msgStart'\`\")
-curl -s -X POST https://api.telegram.org/bot\$API_TOKEN/sendMessage -d chat_id=\$CHAT_ID -d text=\"\$accStart\"
+msgStart=\$(eval \" cat covalent\"\$akun\".log | awk '{print tolower(\\\$0)}' | grep -ow '\w*0x\w*'\")
+accStart=\$(eval \" echo 'Address \$akun : \\\`\$msgStart\\\`'\")
+curl -s -X POST https://api.telegram.org/bot\$API_TOKEN/sendMessage -d chat_id=\$CHAT_ID -d text=\"\$accStart\" -d parse_mode='MarkdownV2'
 done
 
 while sleep 1800;
