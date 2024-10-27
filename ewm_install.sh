68f96c62683d0b19091c9c01b2a48ee84f520f23
Muhammad Ruly <116989757+mr9868@users.noreply.github.com>
2024-10-28 04:35:25 +0700

    Update ewm_install.sh

--- ewm_install.sh
+++ ewm_install.sh
@@ -127,7 +127,7 @@ function tgMsg(){
 # Set the API token and chat ID
 API_TOKEN=\"$tgApiQn\"
 CHAT_ID=\"$tgIdQn\"
-sleep 200;
+sleep 120;
 for akun in \$(seq 1 $loop);
 do  
 MESSAGE=\$(eval \" cat ipfs\"\$akun\".log | grep ready\"); 
