a816ba8c0544329e622995b57b49694c680c17cd
Muhammad Ruly <116989757+mr9868@users.noreply.github.com>
2024-10-29 00:51:04 +0700

    Update ewm_install.sh

--- ewm_install.sh
+++ ewm_install.sh
@@ -86,9 +86,10 @@ read -p "How many light-node do you want to run  : " loop
 done
 for i in $(seq 1 $loop);
 do
+qnPkey=$(eval 'echo -e "Input your \033[1;33m client $i\033[0m hexadecimal Private Keys ( without 0x ) : "')
 myHeader;
 echo "How many light-node do you want to run  : "$loop""
-read -p $(eval 'echo -e "Input your \033[1;33m client $i\033[0m hexadecimal Private Keys ( without 0x ) : "') pkey
+read -p "$qnPkey" pkey
 varInputPkey="pkey$i=$pkey"
 eval $varInputPkey
 varPkey="echo \$pkey$i"
@@ -98,7 +99,7 @@ do
   myHeader;
   echo "How many light-node do you want to run  : "$loop""
   echo "Error: PRIVATE_KEY is not a valid 64-character hexadecimal number."
-  read -p $(eval 'echo -e "Input your \033[1;33m client $i\033[0m hexadecimal Private Keys ( without 0x ) : "') pkey
+  read -p "$qnPkey" pkey
   varInputPkey="pkey$i=$pkey"
   eval $varInputPkey
   varPkey="echo \$pkey$i"
