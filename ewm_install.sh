8f8e62c694b5f064c89ba953e90fa56b91433269
Muhammad Ruly <116989757+mr9868@users.noreply.github.com>
2024-10-30 15:56:33 +0700

    Update ewm_install.sh

--- ewm_install.sh
+++ ewm_install.sh
@@ -48,7 +48,7 @@ command -v go >/dev/null 2>&1 || { echo >&2 "Go is not found on this machine, In
 v=`go version | { read _ _ v _; echo ${v#go}; }`
 IFS="." tokens=( ${v} );
 version=${tokens[1]};
-if (("$version"<"23")); then 
+if (($version<23)); then 
 echo "Your go version '${version}' is outdated, Updating your go ...";sleep 5; installGo;
 else 
 echo "Your go version '${version}' is up to date, Next step ...";sleep 5;
@@ -487,7 +487,7 @@ function notInstalled(){
 # Install ewm-das
      git clone https://github.com/covalenthq/ewm-das  &&
      cd ewm-das &&
-     mkdir ~/ewm-das/.mr9868 &&
+     mkdir ~/ewm-das/.mr9868 
 }
 
 function installer(){
