d60e7df505b06ce05b4c0132c69d50928470710c
Muhammad Ruly <116989757+mr9868@users.noreply.github.com>
2024-10-30 14:21:01 +0700

    Update ewm_install.sh

--- ewm_install.sh
+++ ewm_install.sh
@@ -459,6 +459,14 @@ then
      installer
      fi
    else
+   myHeader
+   mkdir ~/ewm-das/.mr9868
+   echo "Installing required dependencies ..."
+   # sudo bash -c ' >/dev/null 2>&1 & disown' &&
+   sudo apt install screen -y && 
+   sudo apt install git -y &&
+   sudo apt install wget -y &&
+   sudo apt install ufw -y 
    installer
    fi
 else
@@ -469,13 +477,6 @@ fi
 
 function installer(){
 myHeader;
-echo "Installing required dependencies ..."
-# sudo bash -c ' >/dev/null 2>&1 & disown' &&
-sudo apt install screen -y && 
-sudo apt install git -y &&
-sudo apt install wget -y &&
-sudo apt install ufw -y 
-
 if [[ "${dirFound}" =~ ^([yY][eE][sS]|[yY])$ ]];
      then
      echo "Next ..."
