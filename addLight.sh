01bf4c9279f6318bcb9a427e4c272d1d96c88b67
Muhammad Ruly <116989757+mr9868@users.noreply.github.com>
2024-10-30 14:31:38 +0700

    Update addLight.sh

--- addLight.sh
+++ addLight.sh
@@ -1,9 +1,12 @@
+# if have installed directory :
+# check with prompt -> 
+start=$(date -d "-30 minutes" +'%Y-%m-%d %H:%M:%S')
+   awk -v s="$start" 's<$0' covalent1.log
 if [ -f ~/ewm-das ]
 then
    read -p "ewm-das directories found ! do you want to add light-client ? (y/n) : " dirFound
    if [[ "$dirFound" =~ ^([yY][eE][sS]|[yY])$ ]];
    then
-   start=$(date -d "-30 minutes" +'%Y-%m-%d %H:%M:%S')
-   awk -v s="$start" 's<$0' covalent1.log
+   
    fi
 fi
