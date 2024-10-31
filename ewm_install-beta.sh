c10507712250cea5fe4015b974777b1c53d8ebb6
Muhammad Ruly <116989757+mr9868@users.noreply.github.com>
2024-10-31 11:04:08 +0700

    Update ewm_install-beta.sh

--- ewm_install-beta.sh
+++ ewm_install-beta.sh
@@ -158,7 +158,6 @@ varPkey=${privKey[$(((0-1)+${i}))]}
 if [[ "${ipfsQn}" =~ ^([yY][eE][sS]|[yY])$ ]];
 then
 screen -dmS covalent${i} -L -Logfile ${cfgDir}/covalent${i}.log bash -c "sudo light-client --rpc-url wss://coordinator.das.test.covalentnetwork.org/v1/rpc --collect-url https://us-central1-covalent-network-team-sandbox.cloudfunctions.net/ewm-das-collector --ipfs-addr :${mainPort} --private-key ${varPkey} ;exec bash"
-fi
 else
 screen -dmS covalent${i} -L -Logfile ${cfgDir}/covalent${i}.log bash -c "sudo light-client --rpc-url wss://coordinator.das.test.covalentnetwork.org/v1/rpc --collect-url https://us-central1-covalent-network-team-sandbox.cloudfunctions.net/ewm-das-collector --ipfs-addr :${mainPort} --private-key ${varPkey} ;exec bash"
 fi
