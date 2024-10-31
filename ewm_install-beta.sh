03ce7a5c9c611dd558a01bebb7486f39e9854ef7
Muhammad Ruly <116989757+mr9868@users.noreply.github.com>
2024-11-01 04:26:44 +0700

    Update ewm_install-beta.sh

--- ewm_install-beta.sh
+++ ewm_install-beta.sh
@@ -131,7 +131,7 @@ curl -s -X POST https://api.telegram.org/bot\${API_TOKEN}/sendMessage -d chat_id
 sleep 120;
 for akun in \$(seq 1 \${#privKey[@]});
 do  
-MESSAGE=\$(eval \" cat ipfs\${akun}.log | grep ready\"); 
+MESSAGE=\$(eval \" cat \${cfgDir}/ipfs\${akun}.log | grep ready\"); 
 curl -s -X POST https://api.telegram.org/bot\${API_TOKEN}/sendMessage -d chat_id=\${CHAT_ID} -d text=\"\${MESSAGE}\"
 msgStart=\$(eval \" cat \${cfgDir}/covalent\${akun}.log | awk '{print tolower(\\\$0)}' | grep -ow '\w*0x\w*'\")
 accStart=\$(eval \" echo 'Address \${akun} : \\\`\${msgStart}\\\`'\")
