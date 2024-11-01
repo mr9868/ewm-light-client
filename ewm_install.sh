b873c2ec69a669c6ca599c18e6918e63f86caabc
Muhammad Ruly <116989757+mr9868@users.noreply.github.com>
2024-11-01 09:09:11 +0700

    Update ewm_install.sh

--- ewm_install.sh
+++ ewm_install.sh
@@ -442,7 +442,7 @@ rm -rf ${cfgDir}/ipfs${ipfsCount}.log
 screen -dmS ipfs${ipfsCount} -L -Logfile ${cfgDir}/ipfs${ipfsCount}.log bash -c \"IPFS_PATH=${cfgDir}/.ipfs${ipfsCount} ipfs daemon --init;exec bash;\" 
 }
 ipfs${ipfsCount}
-" > ${cfgDir}/ipfs${ipfsCount}.sh
+" >> ${cfgDir}/ipfs${ipfsCount}.sh
 chmod 777  ${cfgDir}/ipfs${ipfsCount}.sh && bash ${cfgDir}/ipfs${ipfsCount}.sh
 # screen -dmS ipfs${ipfsCount} -L -Logfile $cfgDir/ipfs${ipfsCount}.log bash -c "IPFS_PATH=${cfgDir}/.ipfs${ipfsCount} ipfs daemon --init;exec bash;" 
 else
@@ -482,7 +482,7 @@ rm -rf ${cfgDir}/ipfs${ipfsCount}.log
 screen -dmS ipfs${ipfsCount} -L -Logfile ${cfgDir}/ipfs${ipfsCount}.log bash -c \"IPFS_PATH=${cfgDir}/.ipfs${ipfsCount} ipfs daemon --init;exec bash;\" 
 }
 ipfs${ipfsCount}
-" > ${cfgDir}/ipfs${ipfsCount}.sh
+" >> ${cfgDir}/ipfs${ipfsCount}.sh
 chmod 777  ${cfgDir}/ipfs${ipfsCount}.sh && bash ${cfgDir}/ipfs${ipfsCount}.sh
 fi
 }
