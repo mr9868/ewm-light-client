bff88b795e3f8131449c7c69f93d2a195df0786d
Muhammad Ruly <116989757+mr9868@users.noreply.github.com>
2024-11-03 00:06:38 +0700

    Update ewm_install.sh

--- ewm_install.sh
+++ ewm_install.sh
@@ -86,6 +86,7 @@ function entryPointPK(){
 read -p "How many light-node do you want to run  : " loop
 until [[ $loop =~ ^[0-9]+$ ]]
 do
+
 myHeader;
 echo "Error: Please input in number !";
 read -p "How many light-node do you want to run  : " loop
