aaf90612bfef2d7dddfa42bc2373ed3ad0226bdd
Muhammad Ruly <116989757+mr9868@users.noreply.github.com>
2024-11-07 13:44:26 +0700

    Update ewm_install.sh

--- ewm_install.sh
+++ ewm_install.sh
@@ -412,10 +412,9 @@ nodeHealth2=\$(eval \" echo '<b>[ ERROR ]</b> covalent\${svcHealth} doesn't runn
 echo \${nodeHealth2}
 fi
 done
-fi
 );
 curl -s -X POST https://api.telegram.org/bot\${API_TOKEN}/sendMessage -d chat_id=\${CHAT_ID} -d text=\"\${nodeHealth}\" -d parse_mode='HTML'
-
+fi
 
 
 until [ \$mid2 -ne \$mid ]
